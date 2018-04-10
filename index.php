98087247ff12d6d825ee3ee665ff9a290e3ff623
Trevor Wu <trevorwu@1-174-230-253.dynamic-ip.hinet.net>
2018-04-10 22:58:25 +0800

    index update
    Full Page Responsive Background image

--- index.php
+++ index.php
@@ -5,7 +5,42 @@
 </head>
 <body class="wowTitleText">
 	<!--NavBar-->
-	<?php include 'navBarIndex.php' ?>
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark" id="mainNav" style="background-color: #3d3d3d;position:absolute;">
+	<div class="container">
+		<a class="navbar-brand js-scroll-trigger" href="../">No Awful Food</a>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>  
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<!--doorRight-->
+            	<li class="nav-item">
+					<a class="nav-link" href="../location/doorLeft">南台街左邊</a>
+				</li>
+				<!--doorLeft-->
+				<li class="nav-item">
+					<a class="nav-link" href="../location/doorRight">南台街右邊</a>	
+				</li>
+				<!--stRight-->
+				<li class="nav-item">
+					<a class="nav-link" href="../location/stLeft">大武街左邊</a>						
+				</li>
+				<!--stLeft-->
+				<li class="nav-item">
+					<a class="nav-link" href="../location/stRight">大武街右邊</a>					
+				</li>
+				<!--hosp-->
+				<li class="nav-item">
+					<a class="nav-link" href="../location/hosp">往醫院方向</a>
+				</li>
+				<!--other-->
+				<li class="nav-item">
+					<a class="nav-link" href="../location/other">其他</a>
+				</li>
+			</ul>
+        </div>
+	</div>
+</nav>
 	<div class="header-nightsky">
         <div class="hero">
             <h1>找食物?<br>來這邊!</h1>
